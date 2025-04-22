9358b3bf186f09b83344c6080fc417d8ea2553d6
Eduarda <eduarda.damacena@upe.br>
2025-04-22 07:31:27 -0300

    mudei

--- src/main/java/upe/br/Main.java
+++ src/main/java/upe/br/Main.java
@@ -1,4 +1,31 @@
 package upe.br;
 
-public class Main {
-}
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+
+ * Classe que obtém os números do último sorteio da mega-sena.
+
+ */
+
+public class ResultadoMegasena {
+    /** URL que possui as dezenas sorteadas. */
+    private final static String URL =
+
+            "http://www1.caixa.gov.br/_newincludes/home_2011/resultado_megasena.asp
+
+";
+
+    /** Marcação inicial para extrair as dezenas do retorno HTML. */
+    private final static String MARCA_INICIAL_RETORNO_NAO_UTIL = "<div
+
+    id='concurso_resultado'>";
+
+    /** Marcação final para extrair as dezenas do retorno HTML. */
+    private final static String MARCA_FINAL_RETORNO_NAO_UTIL =
+
+            "</div>";
